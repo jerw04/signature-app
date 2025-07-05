8177fe65fd33e9797142907f1f0f8f7d1229ecba
jerw04 <jerwinmanuel04@gmail.com>
2025-07-05 22:47:14 +0530

    Backend ready for Render deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+// client/src/config.js
+const API_BASE = 'https://signature-app-px77.onrender.com';
 export default API_BASE;
